8ca22ce6e8ca81f23b4777b22951bc4ce0fc4a02
Marcus Pascual <marcusjoaquim05@gmail.com>
2026-04-19 17:33:48 +0800

    feat: add empty nav bar

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<div class="justify-between rounded-full border-black border-1 border-black px-2">
+    <a href="/" aria-label="research-questions">Research Questions</a>
+    <a href="/" aria-label="Data"></a>
+    <a href="/" aria-label="Methods"></a>
+    <a href="/" aria-label="research-questions"></a>
+    <a href="/" aria-label="research-questions"></a>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import NavBar from '$lib/components/NavBar.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<div class="p-5">
+<div class="p-5 bg-[#1da1f2]">
+	<header class="z-50 sticky inset-x-0 p-3 top-3">
+		<NavBar />
+	</header>
 	{@render children()}
 </div>
