015a4418b4d19d6c5c24a651e3eb8042297f7ee4
Marcus Pascual <marcusjoaquim05@gmail.com>
2026-04-19 20:05:11 +0800

    add NavBar styles

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -2,10 +2,12 @@
 
 </script>
 
-<div class="justify-between rounded-full border-black border-1 border-black px-2">
-    <a href="/" aria-label="research-questions">Research Questions</a>
-    <a href="/" aria-label="Data"></a>
-    <a href="/" aria-label="Methods"></a>
-    <a href="/" aria-label="research-questions"></a>
-    <a href="/" aria-label="research-questions"></a>
+<div class="bg-jet-black text-light-cyan text-center flex flex-row justify-between w-full shadow-md px-8 py-4">
+    <h1 class="text-xl font-bold">michAIR jURBAN</h1>
+    <div class="flex flex-row gap-8 justify-around">
+        <a href="/" aria-label="research-questions">Research Questions</a>
+        <a href="/" aria-label="hypothesis">Hypothesis</a>
+        <a href="/" aria-label="objectives">Objectives</a>
+        <a href="/" aria-label="data">Data</a>
+    </div>
 </div>
